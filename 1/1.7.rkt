1544b48669dc191ce31887140b24b673899c413d
Roman Makarov <deepspiral@yandex.ru>
2021-10-15 20:24:35 +0300

    solve 1.7

---
+++ 1/1.7.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(define (average x y) (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (/ (abs (- guess
+                (improve guess x)))
+        guess)
+     0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x) guess
+      (sqrt-iter (improve guess x) x)))
+
+(define (sqrt x) (sqrt-iter (/ x 2.0) x))
